91f8ede1e26cc3697df7528d128906812e628239
Ana <bardyana@gmail.com>
2023-03-14 22:05:41 -0300

    Trabajo Final

---
+++ stock.js
@@ -0,0 +1,7 @@
+let stockProductos = [
+    {id: 1, nombre: "Jabón Sun Flower", cantidad: 1, desc: "Jabón Artesanal elaborado con ingredientes naturales bajo la técnica de saponificación.", precio: 1200, img: './img/jab3.png'},
+    {id: 2, nombre: "Jabón Rosas y Amapolas", cantidad: 1, desc: "Jabón Artesanal elaborado con ingredientes naturales bajo la técnica de saponificación.", precio: 1100, img: './img/jab2.png'},
+    {id: 3, nombre: "Jabón Divino Carbón", cantidad: 1, desc: "Jabón Artesanal elaborado con ingredientes naturales bajo la técnica de saponificación.", precio: 1200, img: './img/jab4.png'},
+    {id: 4, nombre: "Jabón Café", cantidad: 1, desc: "Jabón Artesanal elaborado con ingredientes naturales bajo la técnica de saponificación.", precio: 1400, img: './img/jab1.png'},
+    {id: 5, nombre: "Jabón Canela y Jengibre", cantidad: 1, desc: "Jabón Artesanal elaborado con ingredientes naturales bajo la técnica de saponificación.", precio: 1200, img: './img/jab5.png'},
+]
